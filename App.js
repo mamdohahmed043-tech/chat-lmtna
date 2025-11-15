8df201c72c6f7a8656929442b04d8e59b0cc8fd6
mamdohahmed043-tech <mamdohahmed043@gmail.com>
2025-11-15 02:44:02 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <WebView
+      source={{ uri: "https://www.lmtna.chat/ar/" }}
+      style={{ flex: 1 }}
+    />
+  );
+}
